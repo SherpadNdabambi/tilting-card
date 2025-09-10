194cf5fde5d143c1acb6270ddd981cd92f6ee6b9
Sherpad Ndabambi <sgndabambi@gmail.com>
2025-09-11 01:21:17 +0200

    Update landing page

--- assets/ts/code-snippet.ts
+++ assets/ts/code-snippet.ts
@@ -1,4 +1,65 @@
-document.addEventListener("DOMContentLoaded", onLoad);
+let cssTab;
+document.addEventListener("DOMContentLoaded", () => {
+  onLoad();
+
+  // Typescript will throw an error here because it doesn't know
+  // about CodeMirror, but the browser will recognize it since it's
+  // loaded in index.html
+  // @ts-expect-error
+  cssTab = CodeMirror(document.querySelector(".lang-css"), {
+    lineNumbers: true,
+    mode: "css",
+    theme: "dracula",
+    value: `.card {
+  background: rgb(44,62,80);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  color: #fff;
+  padding: 20px;
+  width: 250px;
+}`,
+  });
+
+  const codeSnippet: HTMLDivElement = document.querySelector(".code-snippet"),
+    htmlTab: HTMLDivElement = document.querySelector(".lang-html");
+  // @ts-expect-error
+  CodeMirror(codeSnippet, {
+    lineNumbers: true,
+    mode: "htmlmixed",
+    theme: "dracula",
+    value: `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sherpadndabambi/tilting-card@v1.0.0/assets/css/tilting-card.css"/>
+<script src="https://github.com/sherpadndabambi/tilting-card/releases/download/v1.0.0/tilting-card.js"></script>`,
+  });
+  // @ts-expect-error
+  CodeMirror(htmlTab, {
+    lineNumbers: true,
+    mode: "htmlmixed",
+    theme: "dracula",
+    value: `<head>
+
+  <link rel="stylesheet" href="./assets/css/styles.css" />
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sherpadndabambi/tilting-card@v1.0.0/assets/css/tilting-card.css" />
+
+  <script src="https://github.com/sherpadndabambi/tilting-card/releases/download/v1.0.0/tilting-card.js"></script>
+
+</head>
+
+<body>
+
+  <div class="card highlight-circle tilting-card">
+
+    <h3>Tilting Card</h3>
+
+    <p>
+      JavaScript calculates the mouse position relative to the card's center and uses CSS transforms rotateX and rotateY to tilt the card.
+    </p>
+
+  </div>
+
+</body>`,
+  });
+});
 
 /**
  * Adds click event to tab link elements.
@@ -16,6 +77,7 @@ function addClickEvent(tabLink: Element) {
  */
 function onLoad() {
   const tabLinks: NodeListOf<Element> = document.querySelectorAll(".tab-link");
+
   tabLinks.forEach((tabLink) => {
     addClickEvent(tabLink);
   });
@@ -26,18 +88,24 @@ function onLoad() {
  *
  * @param tab - The tab to select
  */
-function selectTab(tab: string) {
-  const activeTab: HTMLPreElement = document.querySelector(
-      ".code-snippet-window .active.code-snippet"
+function selectTab(tabName: string) {
+  const activeTab: HTMLDivElement = document.querySelector(
+      ".code-snippet-window .active.code-editor"
     ),
     activeTabLink: HTMLSpanElement = document.querySelector(".tab-link.active"),
     clickedTabLink: HTMLSpanElement = document.querySelector(
-      `.tab-link[data-tab="${tab}"]`
+      `.tab-link[data-tab="${tabName}"]`
     ),
-    selectedTab: Element = document.getElementsByClassName(tab)[0];
+    selectedTab: HTMLDivElement = document.getElementsByClassName(
+      tabName
+    )[0] as HTMLDivElement;
 
   activeTab.classList.remove("active");
   activeTabLink.classList.remove("active");
   clickedTabLink.classList.add("active");
   selectedTab.classList.add("active");
+
+  if (selectedTab.classList.contains("lang-css")) {
+    cssTab.refresh();
+  }
 }
